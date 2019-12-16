3a32af651637c260ad8be798809e45d10201fcf8
Michele Cannizzaro <michele.cannizzaro94@gmail.com>
2019-12-16 19:49:04 +0100

    vms - Folder Initialization

---
+++ videomanagementservice/src/main/java/com/dslab/homework1/videomanagementservice/VideomanagementserviceApplication.java
@@ -0,0 +1,13 @@
+package com.dslab.homework1.videomanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideomanagementserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideomanagementserviceApplication.class, args);
+    }
+
+}
