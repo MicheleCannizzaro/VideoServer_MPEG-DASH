a926d073196bbbb027874d276624bd93922887f6
michelecannizzaro <michele.cannizzaro94@gmail.com>
2019-12-20 09:25:30 +0100

    vps -  Fix Start to Deploy

--- videoprocessingservice/src/main/java/com/dslab/homework1/videoprocessingservice/controller/VideoProcessController.java
+++ videoprocessingservice/src/main/java/com/dslab/homework1/videoprocessingservice/controller/VideoProcessController.java
@@ -1,4 +1,28 @@
 package com.dslab.homework1.videoprocessingservice.controller;
 
-public class VideoProcessController {
+import com.dslab.homework1.videoprocessingservice.service.VideoProcessService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping(path = "/videos")
+public class VideoProcessController{
+
+    @Autowired
+    VideoProcessService service;
+
+    //COMPLETARE
+    /*//POST http://localhost:8080/videos/process
+    @PostMapping(path = "/process")
+    public ResponseEntity<String> process (@RequestBody VideoRequest video_request){
+
+            //Sistemare!!!
+
+        if(service.encoding()){
+
+        }
+    }*/
 }
--- videoprocessingservice/src/main/java/com/dslab/homework1/videoprocessingservice/service/VideoProcessService.java
+++ videoprocessingservice/src/main/java/com/dslab/homework1/videoprocessingservice/service/VideoProcessService.java
@@ -1,4 +1,41 @@
 package com.dslab.homework1.videoprocessingservice.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.io.IOException;
+
+@Service
+@Transactional
 public class VideoProcessService {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+    @Autowired
+    private TaskExecutor taskExecutor;
+
+    public boolean encoding() {
+
+        boolean exit=false;
+
+        taskExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+
+                //FFMPEG Bash Script Execution
+                try {
+                    Process process = Runtime.getRuntime().exec("./videoEncoder video.mp4 Storage/var/videofiles ");
+                    //Sistemare
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+        return true;  //solo per non avere errori al momento del commit (da modificare!)
+    }
 }
